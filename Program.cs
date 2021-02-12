09494939e12261ae909d929c386dc4a22f3bb549
Alexander Kalabin <k414bin@yandex.ru>
2021-02-12 14:56:01 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Numerics;
+
+
+namespace Lesson_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добрый день! ");
+            Console.WriteLine("Данная программа рассчитывает для прямоугольного поля размера M на N клеток количество путей из верхней левой клетки в правую нижнюю.");
+            Console.Write("Введите размер поля М: ");
+            int m= Convert.ToInt32(System.Console.ReadLine());
+            Console.Write("Введите размер поля N: ");
+            int n = Convert.ToInt32(System.Console.ReadLine());
+            // проверено тут https://www.wolframalpha.com
+            Console.WriteLine("Количество путей вычисляется по формуле неупорядоченной выборки с повторениями из комбинаторики.");
+            // проверяем по частям 
+            BigInteger result1 = FactTree((m - 1) + n);
+            BigInteger result2 = FactTree(m);
+            BigInteger result3 = FactTree(n-1);
+            // конечное значение
+            BigInteger total = result1 / (result2 * result3);
+            Console.Write("Путей будет ");
+            Console.Write(total);
+            Console.WriteLine();
+            System.Console.ReadLine();
+
+        }
+        // создаем древо для вычислений факториала
+          static  BigInteger ProdTree(int l, int r)
+            {
+                if (l > r)
+                    return 1;
+                if (l == r)
+                    return l;
+                if (r - l == 1)
+                // использую BigInteger из пространства  System.Numeric
+                //возможно переполнение, лучше использовать значения до 20
+                    return (BigInteger)l * r;
+                //уполовиним значение
+                int m = (l + r) / 2;
+            // и раскидаем по веткам в рекурсии
+                return ProdTree(l, m) * ProdTree(m + 1, r);
+            }
+        // Формула факториала
+            static BigInteger FactTree(int n)
+            {
+                if (n < 0)
+                    return 0;
+                if (n == 0)
+                    return 1;
+                if (n == 1 || n == 2)
+                    return n;
+                return ProdTree(2, n);
+            }
+        }
+    }
+
